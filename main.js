13fd19dd5dd824543ef88bdd9889937fa5c7d053
andriykhomik <andriykhomik1992@gmail.com>
2021-12-03 17:04:19 +0200

    optimized

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const btnMagic = document.querySelector('#btn');
 const boxes = document.querySelector('#boxes');
-const quantity = 16;
 
 let positionX = 0;
 let positionY = 0;
 
 
-for (let i = 0; i < quantity; i++) {
+for (let i = 0; i < 16; i++) {
 
    const boxEl = document.createElement('div');
    boxEl.classList.add('box');
-   console.log (positionX);
-   console.log (positionY);
    boxEl.style.backgroundPosition = `${positionX}px ${positionY}px`
+   boxes.appendChild(boxEl);
 
    if ((i+1) % 4 === 0){
       positionX = 0;
@@ -20,12 +18,7 @@ for (let i = 0; i < quantity; i++) {
    }else {
       positionX = positionX -125;
    }
-
-   boxes.appendChild(boxEl);
 }
 
-btnMagic.addEventListener('click', ()=>{
-   boxes.classList.toggle('big');
-})
 
-
+btnMagic.addEventListener('click', ()=> boxes.classList.toggle('big'));
